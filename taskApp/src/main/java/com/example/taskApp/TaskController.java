d393a925bc2cb5fbc12047f09db39edd9d19e385
Marek Pędzisz <pedziszm8@gmail.com>
2024-03-20 15:23:01 +0100

    first  commit

--- taskApp/src/main/java/com/example/taskApp/TaskController.java
+++ taskApp/src/main/java/com/example/taskApp/TaskController.java
@@ -1,2 +1,25 @@
-package com.example.taskApp;public class TaskControler {
+package com.example.taskApp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    @Autowired
+    TaskRepository taskRepository;
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        Task saved = taskRepository.save(task);
+        return saved;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks () {
+        List<Task> taskList = taskRepository.findAll();
+        return taskList;
+    }
 }
