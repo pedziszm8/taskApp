d2c56da5d0125e973795a58f99d973316d07c337
Marek Pędzisz <pedziszm8@gmail.com>
2024-03-26 15:09:20 +0100

    adding update and delete

--- taskApp/src/main/java/com/example/taskApp/TaskController.java
+++ taskApp/src/main/java/com/example/taskApp/TaskController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -18,8 +19,34 @@ public class TaskController {
     }
 
     @GetMapping
-    public List<Task> getAllTasks () {
+    public List<Task> getAllTasks() {
         List<Task> taskList = taskRepository.findAll();
         return taskList;
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+
+
+    }
+
+    @DeleteMapping
+    public void deleteTasks() {
+        taskRepository.deleteAll();
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        Task task1 = taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+        task1.setDescription(task.getDescription());
+        task1.setStatus(task.status);
+        return taskRepository.save(task1);
     }
 }
