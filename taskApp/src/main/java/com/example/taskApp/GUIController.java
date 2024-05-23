88af118ee0b661daed4731405621c09903798286
Marek Pędzisz <pedziszm8@gmail.com>
2024-05-23 16:20:42 +0200

    Added possibility of deleting task in GUI link to adding new task and reload

--- taskApp/src/main/java/com/example/taskApp/GUIController.java
+++ taskApp/src/main/java/com/example/taskApp/GUIController.java
@@ -3,9 +3,7 @@ package com.example.taskApp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,4 +43,13 @@ public class GUIController {
         return "redirect:/taskList";
 
     }
+
+    @PostMapping("/deleteTask/{id}")
+    public String deleteTask(@PathVariable ("id")long id){
+        System.out.println("Id suswanego taska:"+id);
+        taskRepository.deleteById(id);
+        return "redirect:/taskList";
+    }
+
+
 }
